3ea23af676d3b907bbd5a73e8e256345a24de38e
Jan Bianca O. Tejome <111472867+biancatejome@users.noreply.github.com>
2023-05-11 10:05:46 +0800

    Update Maltichon.java

--- exercise3/Dog/Maltichon.java
+++ exercise3/Dog/Maltichon.java
@@ -3,6 +3,6 @@ package Dog;
 class Maltichon extends BichonFrise implements Maltese {
 
     public void bark() {
-        System.out.println("[dog barking] Look, my cute maltichon is barkking!");
+        System.out.println("[dog barking] Look, my cute maltichon is barking!");
     }
 }
