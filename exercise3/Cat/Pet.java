053072b01a46455711c3d126916e628085c78444
Jan Bianca O. Tejome <111472867+biancatejome@users.noreply.github.com>
2023-05-11 10:24:54 +0800

    Update Pet.java

--- exercise3/Cat/Pet.java
+++ exercise3/Cat/Pet.java
@@ -1,13 +1,18 @@
 package Cat;
 public class Pet implements Himalayan {
 
+    String name = "LEO";
+    String age = "4 years old";
         
     @Override
+
     public void meow() {
         System.out.println(" ");
-        System.out.println("[LEO MEOWING: Look at my adorable Himalayan cat seeking attention with his little meows!]");
+        System.out.println("[meows] Look at my adorable Himalayan cat seeking attention with his little meows!]");
         System.out.println(" ");
-        System.out.println("Leo is a mix of Siamese and Persian and has the following characteristics: ");
+        System.out.println("About LEO!");
+        System.out.println(" ");
+        System.out.println("My cat is a mix of Siamese and Persian and has the following characteristics: ");
         System.out.println(" ");
     }
 }
