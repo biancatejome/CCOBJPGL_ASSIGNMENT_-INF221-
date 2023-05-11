612b39d341f54985a307986994a95420ef4c62b7
Jan Bianca O. Tejome <111472867+biancatejome@users.noreply.github.com>
2023-05-11 10:24:19 +0800

    Update App.java

--- exercise3/Cat/App.java
+++ exercise3/Cat/App.java
@@ -4,18 +4,14 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("JIU'S ADORABLE HIMALAYAN CAT!");
-        System.out.println(" ");
-
-        System.out.println("Name: LEO");
-        System.out.println("Age: 4 years old");
-        System.out.println(" ");
-
-        System.out.println("About LEO!");
 
         // Create object from pet class
         Pet myPet = new Pet();
 
         myPet.meow();
+        System.out.println("Name: " + myPet.name);
+        System.out.println("Age: " + myPet.age);
+        System.out.println(" ");
         System.out.println(myPet.eyeColor);
         System.out.println(myPet.furColor);
         System.out.println(myPet.furLength);
